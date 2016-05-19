7d8ed30efd5c4a572e75894e8c22ca56850e41e5
chrisloomis13 <christopherfrenchloomis@gmail.com>
2016-05-18 17:29:35 -0700

    how-to

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+	var cors = require('cors');
 
 // configuration =================
 
