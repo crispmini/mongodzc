b29c1fa928a38b253ef65d1e420eaa0e9d33b6bd
chrisloomis13 <christopherfrenchloomis@gmail.com>
2016-05-22 09:33:04 -0700

    CORS how-to

--- server.js
+++ server.js
@@ -60,7 +60,7 @@
 //app.use(cors());
 
 var corsOptions = {
-	origin : 'http://localhost/',
+	origin : 'http://localhost:8100/',
 	methods : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
 	allowedHeaders : 'Content-Type'
 };
